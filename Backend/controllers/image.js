140425cfa1dc70b7ad00a607c3e17940afe20e44
= <prasham.22bcon114@jecrcu.edu.in>
2024-10-13 16:11:59 +0530

    Changes in profile routes

---
+++ Backend/controllers/image.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/images')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname))
+  }
+})
+
+const upload = multer({ storage })
+
+router.post('/upload', upload.single('file'), (req, res) => {
+  console.log(req.file);
+})
+
+
+module.exports = router;
