bd7a2f15de28c7ed4fc9b7fd836003cf78a8efbc
= <prasham.22bcon114@jecrcu.edu.in>
2024-10-13 13:55:49 +0530

    UserRoutes

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,34 +1,24 @@
-<<<<<<< HEAD
-const mongoose = require('mongoose');
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  profileImg: { type: String }
-});
-=======
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const schema= new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
     profileImg: { type: String }
-   
+
 })
->>>>>>> 440e1138b2cb433ec9ed12f0780e4fc59f596c61
 
-const userModle = mongoose.model('user',schema);
+const userModle = mongoose.model('user', schema);
 
-module.exports=userModle;
+module.exports = userModle;
 
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,29 +1,19 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-const { signup, login } = require('../controllers/userControllers');
-const User = require('../models/user')
-
-//  documents
-var path = require('path');
-=======
-const express=require("express");
-const userRouter=express.Router();
-const {signUp,login,signOut, editName, getUser, emailVerifiction, editEmail}=require("../controllers/userControllers");
+const express = require("express");
+const userRouter = express.Router();
+const { signUp, login, signOut, editName, getUser, emailVerifiction, editEmail } = require("../controllers/userControllers");
 
 
-userRouter.post('/signUp',signUp);
+userRouter.post('/signUp', signUp);
 
-userRouter.post('/login',login);
-userRouter.post('/signout',signOut)
-userRouter.patch('/UpdateName/:email',editName);
-userRouter.get('/getUser/:email',getUser);
-userRouter.patch('/EmailVerify/:email/:otp',emailVerifiction);
-userRouter.patch('/editEmail/:email',editEmail);
+userRouter.post('/login', login);
+userRouter.post('/signout', signOut)
+userRouter.patch('/UpdateName/:email', editName);
+userRouter.get('/getUser/:email', getUser);
+userRouter.patch('/EmailVerify/:email/:otp', emailVerifiction);
+userRouter.patch('/editEmail/:email', editEmail);
 
-module.exports=userRouter;
+module.exports = userRouter;
 
->>>>>>> 440e1138b2cb433ec9ed12f0780e4fc59f596c61
 
 
 
