71d01c0602876b5236c819be00666545e3588927
jenisji <jenish.1442004@gmail.com>
2024-10-15 22:10:26 +0530

    removed bugged

--- Frontend/schedule-manager/src/Components/NavBar.js
+++ Frontend/schedule-manager/src/Components/NavBar.js
@@ -111,6 +111,7 @@ function NavBar({ activeSection, setActivesection }) {
             >
               <img className='h-8 w-8 rounded-full mr-3' src={defaultimg}></img>
             </button>
+            <div>uiewghoidjp</div>
             <button
                   onClick={handleLogout}
                   className="text-white hover:text-red-500 transition-all duration-300"
