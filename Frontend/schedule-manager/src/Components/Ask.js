794ab97c943eb84098e2ae9bbb5233cbd314a0e5
Rishi Jain <jainrishirich2004@gmail.com>
2024-10-17 10:59:30 +0530

    adding getPath

--- Frontend/schedule-manager/src/Components/Ask.js
+++ Frontend/schedule-manager/src/Components/Ask.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { IoMdSend } from "react-icons/io";
 
 const Ask = () => {
   return (
     <div>
+      <div>
+        <form>
+          <input  type='text' placeholder='Enter Your Question Here'/>
+          <button>
+            <IoMdSend/>
+          </button>
+        </form>
 
-      Under Construction
+      </div>
+
+      <div>
+        
+      </div>
+
+      
     </div>
   )
 }
